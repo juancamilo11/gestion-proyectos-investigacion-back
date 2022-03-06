7da15b45438d9065274fcf12c795f63a5c36d849
Joan Rivas <dairo.rivas@udea.edu.co>
2022-03-06 16:53:56 -0500

    Creación proyecto base con Spring, para backend

---
+++ src/main/java/co/edu/udea/practicafinal/GestionProyectosInvestigacionBackApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.practicafinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProyectosInvestigacionBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionProyectosInvestigacionBackApplication.class, args);
+    }
+
+}
