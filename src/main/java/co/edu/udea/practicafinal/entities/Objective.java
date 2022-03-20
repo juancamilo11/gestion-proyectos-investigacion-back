1365ba50c6e3edda7d3c604f3f5f6b66192ec8f4
Oscar Darío Botero Vargas <odbotero@gmail.com>
2022-03-20 17:13:35 -0500

    Configuración para conectarse a MongoDB.

--- src/main/java/co/edu/udea/practicafinal/entities/Objective.java
+++ src/main/java/co/edu/udea/practicafinal/entities/Objective.java
@@ -5,11 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 
@@ -17,10 +12,7 @@ import javax.validation.constraints.NotBlank;
 @Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name="objective")
 public class Objective {
-    @Id
     @NotBlank private String id;
     @NotBlank private String description;
     @NotBlank private Boolean completed;
