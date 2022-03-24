d32fae4e1ba5e743276b8ea40c586bf26c99f892
Johan Sebastian Muentes Casadiego <johan.muentes@udea.edu.co>
2022-03-23 22:05:02 -0500

    Se documentó el proyecto en la parte de mappers y repository

--- src/main/java/co/edu/udea/practicafinal/repositories/ResearcherRepository.java
+++ src/main/java/co/edu/udea/practicafinal/repositories/ResearcherRepository.java
@@ -5,8 +5,15 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+/**
+ * Interfaz que servirá para mapear la entidad
+ */
 @Repository
 public interface ResearcherRepository extends CrudRepository<Researcher, String> {
+    /**
+     * Metodo para buscar el correo del usuario
+     * @param email
+     * @return
+     */
     Optional<Researcher> findByEmail(String email);
 }
